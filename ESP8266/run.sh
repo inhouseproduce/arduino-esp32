88df0ba573e1869a27c956e6760c6d6da06de3c9
Nathan Augusto Tjoar <nathan.tjoar@gmail.com>
2020-01-14 15:11:39 -0800

    Delete run.sh

--- ESP8266/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo pip install pyserial
-sudo pip install pyparsing
-sudo pip install esptool
-sudo pip install virtualenv
-sudo pip install adafruit-ampy
-sudo brew install cmake ninja
-sudo brew install ccacheP
